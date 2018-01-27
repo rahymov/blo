8b8b602797dd58f6df07c8d0a58e5747edeaba88
Yazgylych <y.rahymov@gmail.com>
2018-01-27 12:58:56 -0500

    Styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+
+	root "pages#home"
   resources :articles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/home', to: 'pages#home'
+  get '/about', to: 'pages#about'
+
 end
