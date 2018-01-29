8c16fa079f42cbc69abe85558391bb63e7c3cbf3
Yazgylych <y.rahymov@gmail.com>
2018-01-29 13:16:02 -0500

    Added logout login signup to navigation

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   get '/home', to: 'pages#home'
   get '/about', to: 'pages#about'
   get 'signup', to: 'users#new'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
