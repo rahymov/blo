365246eea9b21f743a85235d5f6976f89c7160a1
Yazgylych <y.rahymov@gmail.com>
2018-02-04 16:41:48 -0500

    Updated navbar category nav

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,18 +2,24 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
+	def setup
+		@user = User.create(username: "joe", email: "joe@example.com", password: "password", admin: true)
+	end
+
 	test "get new category form and create category" do
+		sign_in_as(@user, "password")
 		get new_category_path
 		assert_template "category/new"
 		assert_difference 'Category.count', 1 do
-			post categories_path, params: { category: {name: "Health"}}
-			follow_redirect!
+		post categories_path, params: { category: {name: "Health"}}
+		follow_redirect!
 		end
 		assert_template "categories/index"
 		assert_mathces "Health", response.body
 	end
 
 	test "invalid category submission result in failure" do
+		sign_in_as(@user, "password")
 		get new_category_path
 		assert_template "category/new"
 		assert_no_difference 'Category.count' do
